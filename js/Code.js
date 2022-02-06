e319f01c925bc2f5ac17bcb0e08d30ebf69a306c
ShagunBuraniya <79150396+ShagunBuraniya@users.noreply.github.com>
2022-02-06 22:01:28 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,22 @@
+const accessCode1 = "Rajendra Prasad";
+const accessCode2 = "Mahatma Gandhi";
+const accessCode3 = "Narendra Modi";
+
+function clues() {
+    
+    fill("white")
+    textSize(20)
+    text("Question : Who is first president of india", 120,100)
+    fill("lightblue")
+
+    fill("white")
+    textSize(20)
+    text("Question : Who is known as Father of Nation", 580,170)
+    fill("lightblue")
+
+    fill("white")
+    textSize(20)
+    text("Question : Who is present PrimeMinister of India", 120,300)
+    fill("lightblue")
+
+}
